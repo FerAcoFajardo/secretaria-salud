685d50671cfba8e6e9d06eae7e51c1951202f35c
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-05-02 01:00:50 -0700

    Change in allowed hosts

--- secretaria_salud/settings.py
+++ secretaria_salud/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://secretaria-salud.herokuapp.com', 'https://secretaria-salud.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://secretaria-salud.herokuapp.com', 'https://secretaria-salud.herokuapp.com/', 'secretaria-salud.herokuapp.com']
 
 
 # Application definition
